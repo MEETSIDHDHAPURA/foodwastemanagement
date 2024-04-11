588025e8750b8742d77c05e8cb5a1f18e8c7b3eb
dhrumil9302 <suthardhrumil5@gmail.com>
2024-04-11 17:16:06 +0530

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,7 @@ const Contact = () => {
         color={"white"}
         alignItems={"center"}
       >
+        
         <Typography fontSize={"50px"} fontWeight={"700"}>
           Contact
         </Typography>
